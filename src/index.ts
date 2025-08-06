1516911eb6b618d37aa7ba62694b71f7410c9d9a
Kaleab Mesfin <42962516+Kaleab41@users.noreply.github.com>
2025-08-06 16:48:20 +0300

    feat: added auth middleware for betteauth

--- src/index.ts
+++ src/index.ts
@@ -1,18 +1,33 @@
 import { toNodeHandler } from "better-auth/node";
-import { auth } from "../utils/auth.js";
+import auth from "../utils/auth.js";
 import express from "express";
+import { isAuthenticated } from "../middleware/isAuthenticated.js";
+// const session = require("express-session");
+// import session from "express-session";
+import cookieParser from "cookie-parser";
 const app = express();
 const port = 3000;
 const Version = "V1";
+
+// app.use(
+//   session({
+//     secret: process.env.BOT_TOKEN,
+//     resave: false,
+//     saveUninitialized: false,
+//     cookie: { secure: false },
+//   })
+// );
+
+app.all("/api/auth/*splat", toNodeHandler(auth));
+app.use(cookieParser());
 app.get("/", (req: any, res: any) => {
+  console.log(req.cookies);
   res.send("Hello");
 });
 
-app.all("/api/auth/*splat", toNodeHandler(auth));
-
 app.use(express.json());
 
-app.post(`/${Version}/RegisterProduct`, (req, res) => {
+app.post(`/${Version}/RegisterProduct`, isAuthenticated, (req, res) => {
   res.send("Hey");
 });
 
